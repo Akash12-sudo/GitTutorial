b448e97c68c2bf5fad16a45ac8734bcfbdef4a7d
Akash <akash123@gmail.com>
2023-06-07 12:37:24 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum{
+
+
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 5;
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum: " + (a+b));
+    }
+}
