921b696ef902826294497568853a8732cb4bbf73
Akash <akash123@gmail.com>
2023-06-07 12:43:56 +0530

    added Diff.java

---
+++ Diff.java
@@ -0,0 +1,12 @@
+public class Diff{
+
+
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 5;
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Difference: " + (a-b));
+    }
+}
